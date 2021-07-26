372a7c6740f1add39d2a1aa2de0f04c97ebbe5e2
vincentbaylon <vincentbaylon@gmail.com>
2021-07-26 10:19:42 -0700

    commit tree ae94f40fd357a90f6434c6006b3a63bc7854a554

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header({ isDarkMode, onDarkModeClick }) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header
